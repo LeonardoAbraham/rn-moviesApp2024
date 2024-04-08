76232982a5bafa3b7069f449091436b35cd38b9f
Leonardo Alonzo <leonardo.a@cliker.mx>
2024-04-08 16:55:28 -0600

    Configuración de eslintrc y prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+    root: true,
+    extends: ['@react-native', 'prettier'],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+                'no-inline-styles': false,
+                singleQuote: true,
+                parser: 'flow',
+            },
+        ],
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react-native/no-inline-styles': 'off',
+        'eol-last': 0,
+        'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+        '@typescript-eslint/no-unused-vars': 'off',
+        'react-hooks/exhaustive-deps': 'off',
+        'react/no-unstable-nested-components': [
+            'off' | 'warn' | 'error',
+            {
+                allowAsProps: true | false,
+                customValidators:
+                    [] /* optional array of validators used for propTypes validation */,
+            },
+        ],
+    },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,21 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+    /* arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    useTabs: true,
+    tabWidth: 2, */
+
+    "singleQuote": true,
+    "printWidth": 80,
+    "editor.formatOnSave": true,
+    "proseWrap": "always",
+    "tabWidth": 4,
+    "requireConfig": false,
+    "useTabs": false,
+    "trailingComma": "all",
+    "bracketSpacing": true,
+    "jsxBracketSameLine": false,
+    "semi": true
 };
