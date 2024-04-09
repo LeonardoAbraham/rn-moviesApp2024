2094304f8f4f509b34581ae375bfd07b38ec09cd
Leonardo Alonzo <leonardo.a@cliker.mx>
2024-04-09 10:53:12 -0600

    Navagación entre pantallas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { View, Text } from 'react-native';
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { Navigation } from './presentation/navigation/Navigation';
 
 export const App = () => {
     return (
-        <View>
-            <Text>Movies App</Text>
-        </View>
+        <NavigationContainer>
+            <Navigation />
+        </NavigationContainer>
     );
 };
