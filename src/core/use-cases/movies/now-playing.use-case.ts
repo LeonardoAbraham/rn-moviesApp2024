dada12434efa24cc64b1db8e42ee0dbd5d8ae21f
Leonardo Alonzo <leonardo.a@cliker.mx>
2024-04-17 15:20:31 -0600

    Patrón Mapper - MovieMapper

--- src/core/use-cases/movies/now-playing.use-case.ts
+++ src/core/use-cases/movies/now-playing.use-case.ts
@@ -1,5 +1,6 @@
 import { HttpAdapter } from '../../../config/adapters/http/http.adapter';
 import { NowPlayingResponse } from '../../../infrastructure/interfaces/movie-db-responses';
+import { MovieMapper } from '../../../infrastructure/mappers/movie.mapper';
 import type { Movie } from '../../entities/movie.entity';
 
 export const moviesNowPlayingUseCase = async (
@@ -9,8 +10,7 @@ export const moviesNowPlayingUseCase = async (
         const nowPlaying = await fetcher.get<NowPlayingResponse>(
             '/now_playing',
         );
-        console.log({ nowPlaying });
-        return [];
+        return nowPlaying.results.map(MovieMapper.fromMovieDBResultToEntity);
     } catch (error) {
         console.log(error);
         throw new Error('Error fetching movies - NowPlaying');
