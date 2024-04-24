3b7b250c2172a148b146ba014fef4d5cafa20eed
Leonardo Alonzo <leonardo.a@cliker.mx>
2024-04-24 12:44:40 -0600

    Avance Carrusel de posters

---
+++ src/presentation/components/movies/PosterCarousel.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native';
+import { Movie } from '../../../core/entities/movie.entity';
+import { ScrollView } from 'react-native-gesture-handler';
+import { MoviePoster } from './MoviePoster';
+
+interface Props {
+    movies: Movie[];
+    height?: number;
+}
+
+export const PosterCarousel = ({ height = 440, movies }: Props) => (
+    <View style={{ height }}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {movies.map((movie) => (
+                <MoviePoster key={movie.id} movie={movie} />
+            ))}
+        </ScrollView>
+    </View>
+);
