65c9c3ae861c2fb1c8f7cfdeb11ef52b6076e5b2
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-04-25 09:53:00 -0600

    Información de la pelicula por ID

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,6 +1,16 @@
+import { StackScreenProps } from '@react-navigation/stack';
 import { View, Text } from 'react-native';
+import { RootStackParams } from '../../navigation/Navigation';
+//import { useRoute } from '@react-navigation/native';
 
-export const DetailsScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
+
+export const DetailsScreen = ({ route }: Props) => {
+    const { movieId } = route.params;
+    //const { movieId } = useRoute().params;
+
+    console.log(movieId);
+
     return (
         <View>
             <Text>DetailsScreen</Text>
